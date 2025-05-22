54daacba673f99b2eaf040f871502e0b1c01d58c
JaviVega2810 <jgv283@gmail.com>
2025-05-22 17:15:28 -0600

    added first part to configure

---
+++ ExampleAPI/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExampleAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("testing")]
+        public IActionResult GetTestingData()
+        {
+            _logger.LogInformation("API executed succesfully...");
+
+            return Ok();
+        }
+    }
+}
